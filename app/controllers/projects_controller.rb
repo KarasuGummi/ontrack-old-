ce35992600a93e7e2429a8202c9cabe1040ac980
kevin collier <collier.kevin.m@gmail.com>
2023-08-05 12:53:23 +0900

    created show action with strong params and view page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,14 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :deadline, :category)
+  end
 end
