5d83a1aab4bfabafe2ac6be0dced0e2b6e69acd3
kevin collier <collier.kevin.m@gmail.com>
2023-08-05 13:56:40 +0900

    createa a dashboard actino in the projects controller but still needs work

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,6 +26,15 @@ class ProjectsController < ApplicationController
     redirect_to projects_path, status: :see_other
   end
 
+  def dashboard
+    @recommended_projects = Project.where(category: user.interest)
+    @project = Project.find(current_user.project_id)
+    # I think we can use this line below to access the buddy's name, love, etc.
+    @buddy = current_user.buddy
+    # Not so sure how we will do this right now.
+    # @progress =
+  end
+
   private
 
   def project_params
