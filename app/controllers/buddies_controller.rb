d2af5bd6886f77e6ee716f9cfc5fc1e95f70f243
Favio Valero <favio102@protonmail.com>
2023-08-04 23:11:47 -0700

    buddies controller updated

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,2 +1,44 @@
 class BuddiesController < ApplicationController
+  def index
+    @buddies = Buddie.all
+  end
+
+  def new
+    @buddie = Buddie.new
+  end
+
+  def show
+    @buddie = Buddie.find(params[:id])
+  end
+
+  def create
+    @buddie = Buddie.new(buddie_params)
+    if @buddie.save
+      redirect_to buddies_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @buddie = current_user.buddies.find(params[:id])
+
+    if @buddie.update(buddie_params)
+      redirect_to buddies_path, notice: 'Buddie was updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @buddie = Buddie.find(params[:id])
+    @buddie.destroy
+    redirect_to buddies_path
+  end
+
+  private
+
+  def buddie_params
+    params.require(:buddie).permit(:name, :personality, :image_url)
+  end
 end
