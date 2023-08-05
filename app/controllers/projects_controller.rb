fa7fc09104a7592667f605daefc0069a1f9f039a
kevin collier <collier.kevin.m@gmail.com>
2023-08-05 13:00:23 +0900

    added destroy method to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,10 +14,16 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     if @project.save
-      redirect_to projects
+      redirect_to projects_path
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, status: :see_other
   end
 
   private
